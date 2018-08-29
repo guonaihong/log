3e801d175f171c5b45781fdc8f87c2d000df36b3
guonaihong <guonaihong@qq.com>
2018-08-29 21:56:32 +0800

    update code

--- file.go
+++ file.go
@@ -1,9 +1,57 @@
 package log
 
+import (
+	"os"
+	"sync"
+)
+
+const ErrLimit = errors.New("The length of the written data exceeds the limit")
+
+const defaultPrefix = "default-"
+
 type File struct {
-	MaxSize  int
-	Compress string
+	Compress  string
+	MaxSize   int
+	MaxArchve int
+
+	fd *os.File
+	sync.Mutex
+}
+
+func NewFile(prefix, compress string, maxSize, maxArchive int) *File {
+	if compress == "" {
+		compress = "gz"
+	}
+
+	if prefix == "" {
+		prefix = defaultPrefix
+	}
+
+	return &File{
+		Compress:   compress,
+		prefix:     prefix,
+		MaxSize:    maxSize,
+		maxArchive: maxArchive,
+	}
+}
+
+func (f *File) checkSize(b []byte) error {
+	sb := f.fd.Stat()
+	if sb.Size() + len(b) {
+	}
+}
+
+func (f *File) openNew() {
 }
 
 func (f *File) Write(b []byte) (n int, err error) {
+	f.Lock()
+	defer f.Unlock()
+
+	if len(b) > f.MaxSize {
+		return 0, ErrLimit
+	}
+}
+
+func (f *File) Close() {
 }
